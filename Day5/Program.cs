95db5b4b8f2e0a80685045f07bb9f80b071d15a0
Chris Roberts <chris@solutionmania.com>
2025-12-19 19:55:41 +0000

    Completed Day 5, Part 2

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Day5;
+﻿using System.Runtime.CompilerServices;
+
+namespace Day5;
 
 class Program
 {
@@ -42,6 +44,51 @@ class Program
         }
 
         Console.WriteLine($"Total number of fresh ingredients: {freshCount}");
+
+        // Part 2... Reduce the ranges so that they don't overlap
+        for (int x = 0; x < freshRanges.Count; x++)
+        {
+            for (int y = 0; y < freshRanges.Count; y ++)
+            {
+                // Make sure we don't compare a range with itself
+                if (x == y)
+                {
+                    continue;
+                }
+
+                // If there is no overlap, we have nothing to do
+                if (!freshRanges[x].OverlapsWith(freshRanges[y]))
+                {
+                    continue;
+                }
+
+                // The ranges overlap - adjust the second range such that they don't
+                if (freshRanges[x].EntirelyContains(freshRanges[y]))
+                {
+                    // The second range is entirely consumed by the first, so we can remove it
+                    freshRanges.RemoveAt(y);
+                }
+                else if (freshRanges[x].Contains(freshRanges[y].LowValue) 
+                         && !freshRanges[x].Contains(freshRanges[y].HighValue))
+                {
+                    // The second range overlaps the top end of this range
+                    freshRanges[y].LowValue = freshRanges[x].HighValue + 1;
+                }
+                else if (freshRanges[x].Contains(freshRanges[y].HighValue)
+                         && !freshRanges[x].Contains(freshRanges[y].LowValue))
+                {
+                    // The second range overlaps the bottom end of this range
+                    freshRanges[y].HighValue = freshRanges[x].LowValue - 1;
+                }
+
+                // There is another possible overlap scenario - the first range entirely consumed by the second.
+                // But we don't need to worry about this as we'll eventually perform the removal the other way around.
+            }
+        }
+
+        long totalFresh = freshRanges.Sum(r => r.Size);
+
+        Console.WriteLine($"Total number of fresh products {totalFresh}");
     }
 
     private class Range
@@ -65,5 +112,9 @@ class Program
         {
             return (value >= LowValue && value <= HighValue);
         }
+
+        public bool OverlapsWith(Range otherRange) => (HighValue >= otherRange.LowValue && LowValue <= otherRange.HighValue);
+        public bool EntirelyContains(Range otherRange) => (otherRange.LowValue >= LowValue && otherRange.HighValue <= HighValue);
+        public long Size => HighValue - LowValue + 1;
     }
 }
