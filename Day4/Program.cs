a49c2daa63cae4a006f6ded5dd1d9b4bc2dceb68
Chris Roberts <chris@solutionmania.com>
2025-12-19 18:39:41 +0000

    Completed Day 4, Part 2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
 
 namespace Day4;
 
@@ -9,70 +10,98 @@ class Program
 
     static void Main(string[] args)
     {
-        List<char[]> paperRows = [];
+        int result = 0;
+        int rollsRemovedThisRound = 0;
 
-        int row = 0;
-
+        // Retrieve all of the source data
+        List<char[]> rollMap = [];
         foreach (string currentRow in File.ReadLines(inputFilename))
         {
-            // First time in, add an extra row. We'll always store one more row than we have read from the file
-            if (row == 0)
-            {                
-                char[] newRow = new char[currentRow.Length];
-                paperRows.Add(newRow);
-            }
+            rollMap.Add(currentRow.ToCharArray());
+        }
 
-            // Add a row after the current row
-            paperRows.Add(new char[currentRow.Length]);
+        do
+        {
+            List<char[]> paperRows = [];
 
-            for (int col = 0; col < currentRow.Length; col ++)
+            int row = 0;
+
+            foreach(char[] currentRow in rollMap)
             {
-                if (currentRow[col] == '@')
+                // First time in, add an extra row. We'll always store one more row than we have read from the file
+                if (row == 0)
+                {                
+                    char[] newRow = new char[currentRow.Length];
+                    paperRows.Add(newRow);
+                }
+
+                // Add a row after the current row
+                paperRows.Add(new char[currentRow.Length]);
+
+                for (int col = 0; col < currentRow.Length; col ++)
                 {
-                    // Increment values in a 3x3 grid around the current coordinates, apart from the current coordinate
-                    for (int x = col - 1; x <= col + 1; x ++)
+                    if (currentRow[col] == '@')
                     {
-                        for (int y = row - 1; y <= row + 1; y ++)
+                        // Increment values in a 3x3 grid around the current coordinates, apart from the current coordinate
+                        for (int x = col - 1; x <= col + 1; x ++)
                         {
-                            if (x >= 0 
-                                && x < currentRow.Length 
-                                && y >= 0
-                                && !(x == col && y == row)
-                                && paperRows[y][x] != '.')
+                            for (int y = row - 1; y <= row + 1; y ++)
                             {
-                                paperRows[y][x]++;
+                                if (x >= 0 
+                                    && x < currentRow.Length 
+                                    && y >= 0
+                                    && !(x == col && y == row)
+                                    && paperRows[y][x] != '.')
+                                {
+                                    paperRows[y][x]++;
+                                }
                             }
                         }
                     }
+                    else
+                    {
+                        paperRows[row][col] = '.';
+                    }
                 }
-                else
-                {
-                    paperRows[row][col] = '.';
-                }
+
+                row++;
             }
 
-            row++;
-        }
+            // Remove the final row of data as this has extraneous 'overflow' data in it
+            paperRows.RemoveAt(paperRows.Count - 1);
 
-        // Remove the final row of data as this has extraneous 'overflow' data in it
-        paperRows.RemoveAt(paperRows.Count - 1);
-
-        // Display the accessible rolls
-        foreach (char[] currentRow in paperRows)
-        {
-            string currentRowData = string.Empty;
-
-            foreach (char currentRoll in currentRow)
+            // Display the accessible rolls (and remove them from the map)
+            rollsRemovedThisRound = 0;
+            for (int currentRow = 0; currentRow < paperRows.Count; currentRow ++)
             {
-                currentRowData += (currentRoll < accessibleLimit) ? 'x' : ' ';
+                string currentRowData = string.Empty;
+
+                for (int currentCol = 0; currentCol < paperRows[currentRow].Length; currentCol++)
+                {
+                    char currentRoll = paperRows[currentRow][currentCol];
+
+                    if (currentRoll < accessibleLimit)
+                    {
+                        currentRowData += 'x';
+                        rollMap[currentRow][currentCol] = '.';
+                        rollsRemovedThisRound++;
+                    }
+                    else
+                    {
+                        currentRowData += ' ';                    
+                    }
+                }
+
+                Console.WriteLine(currentRowData);
             }
 
-            Console.WriteLine(currentRowData);
+            // Count the available rolls of paper...
+            result += paperRows.Sum(r => r.Count(p => p < accessibleLimit));
+
+            Console.WriteLine($"Rolls removed this round: {rollsRemovedThisRound}");
         }
+        while (rollsRemovedThisRound > 0);
 
-        // Count the available rolls of paper...
-        int result = paperRows.Sum(r => r.Count(p => p < accessibleLimit));
-
-        Console.WriteLine($"Accessible rolls of paper: {result}");
+        Console.WriteLine($"Total rolls removed: {result}");
     }
 }
